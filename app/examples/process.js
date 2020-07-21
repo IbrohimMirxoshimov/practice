8a5c70a9270dd0c3daa81daa05fdddea4a015b7f
IbrohimAli <imirxoshimov@gmail.com>
2020-07-21 20:32:04 +0500

    some changes

--- app/examples/process.js
+++ app/examples/process.js
@@ -5,23 +5,27 @@ let line = {
 	free: "⬜️"
 }
 let n = 10
-let process_array = Array(n).fill(line.free)
 bot.command('process', ctx => {
-	ctx.reply(process_array.join(""))
-	startProcess(ctx)
+	if (!ctx.session.ps) {
+		ctx.session.ps = true
+		ctx.session.p_array = Array(n).fill(line.free)
+		ctx.reply(ctx.session.p_array.join("")).then(res => {
+			startProcess(ctx, res.message_id)
+		})
+	}
 })
 
-function startProcess(ctx) {
+function startProcess(ctx, mid) {
 	let t = 1
 	let i = setInterval(async () => {
 
-		process_array[t - 1] = line.fill
-		await bot.telegram.editMessageText(ctx.chat.id, ctx.message.message_id + 1, null, process_array.join(""))
+		ctx.session.p_array[t - 1] = line.fill
+		await bot.telegram.editMessageText(ctx.chat.id, mid, null, ctx.session.p_array.join(""))
 		if (t > 9) {
 			clearInterval(i)
 			bot.telegram.sendMessage(ctx.chat.id, "Processing is finished")
+			ctx.session.ps = false
 		}
 		t++
-
 	}, 1000)
 }
